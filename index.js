13be058e72ceb996311bd3c3330ea6ec774b134a
Konstantin Kulebaev <ku.kulebaev@interfax.ru>
2024-04-21 02:22:42 +0400

    style: prettier fmt exec

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
 #!/usr/bin/env node
 
-"use strict";
+'use strict'
 
-const inquirer = require("inquirer");
-const chalk = require("chalk");
-const data = require("./data.json");
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const data = require('./data.json')
 
 // add response color
-const response = chalk.bold.blue;
+const response = chalk.bold.blue
 
 const resumeOptions = {
-    type: "list",
-    name: "resumeOptions",
-    message: "What do you want to know",
-    choices: [...Object.keys(data), "Exit"]
-};
+  type: 'list',
+  name: 'resumeOptions',
+  message: 'What do you want to know',
+  choices: [...Object.keys(data), 'Exit'],
+}
 
 function showResume() {
-    console.log("Hello, this is my resume");
-    handleResume();
+  console.log('Hello, this is my resume')
+  handleResume()
 }
 
 function handleResume() {
-    inquirer.prompt(resumeOptions).then(answer => {
-        if (answer.resumeOptions == "Exit") return;
+  inquirer
+    .prompt(resumeOptions)
+    .then(answer => {
+      if (answer.resumeOptions == 'Exit') return
 
-        const options = data[`${answer.resumeOptions}`]
-        if (options) {
-            console.log(response(new inquirer.Separator()));
-            options.forEach(info => {
-                console.log(response("|   => " + info));
-            });
-            console.log(response(new inquirer.Separator()));
-        }
+      const options = data[`${answer.resumeOptions}`]
+      if (options) {
+        console.log(response(new inquirer.Separator()))
+        options.forEach(info => {
+          console.log(response('|   => ' + info))
+        })
+        console.log(response(new inquirer.Separator()))
+      }
 
-        inquirer
-            .prompt({
-                type: "list",
-                name: "exitBack",
-                message: "Go back or Exit?",
-                choices: ["Back", "Exit"]
-            }).then(choice => {
-            if (choice.exitBack == "Back") {
-                handleResume();
-            } else {
-                return;
-            }
-        });
-    }).catch(err => console.log('Ooops,', err))
+      inquirer
+        .prompt({
+          type: 'list',
+          name: 'exitBack',
+          message: 'Go back or Exit?',
+          choices: ['Back', 'Exit'],
+        })
+        .then(choice => {
+          if (choice.exitBack == 'Back') {
+            handleResume()
+          } else {
+            return
+          }
+        })
+    })
+    .catch(err => console.log('Ooops,', err))
 }
 
-showResume();
+showResume()
